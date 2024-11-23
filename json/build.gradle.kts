b17fe3ccd9a25107c7962e2470f45c252aa34d2e
admin <admin@premsan.com>
2024-11-23 22:41:54 +0800

    initial commit

---
+++ json/build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    java
+    alias(libs.plugins.org.springframework.boot)
+    alias(libs.plugins.io.spring.dependency.management)
+    alias(libs.plugins.com.diffplug.spotless)
+}
+
+group = "com.featurerich"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(":ui"))
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+
+    compileOnly("org.projectlombok:lombok")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    annotationProcessor("org.projectlombok:lombok")
+
+
+    testImplementation(platform(libs.org.junit.junit.bom))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+spotless {
+    format("html") {
+        val htmlTabWidth: Int by rootProject.extra
+        prettier().config(mapOf("tabWidth" to htmlTabWidth))
+
+        target("src/**/templates/**/*.html")
+    }
+
+    java {
+        val googleJavaFormatVersion: String by rootProject.extra
+
+        googleJavaFormat(googleJavaFormatVersion).aosp().reflowLongStrings().skipJavadocFormatting()
+        formatAnnotations()
+    }
+}
