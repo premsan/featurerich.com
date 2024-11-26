12c51462022731fb7073f2336b4b0f059d3b758a
admin <admin@premsan.com>
2024-11-27 03:05:54 +0800

    initial commit

--- ui/src/main/java/com/featurerich/ui/UIConfigurationProperties.java
+++ ui/src/main/java/com/featurerich/ui/UIConfigurationProperties.java
@@ -1,5 +1,6 @@
 package com.featurerich.ui;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import lombok.Getter;
@@ -14,4 +15,16 @@ import org.springframework.context.annotation.Configuration;
 public class UIConfigurationProperties {
 
     private Map<String, List<String>> links;
+
+    public List<String> getAllLinks() {
+
+        final List<String> allLinks = new ArrayList<>();
+
+        for (final Map.Entry<String, List<String>> listEntry : links.entrySet()) {
+
+            allLinks.addAll(listEntry.getValue());
+        }
+
+        return allLinks;
+    }
 }
