9d603a351fa181b4b6950471a553b4bbe16fbff9
admin <admin@premsan.com>
2024-12-02 01:37:49 +0800

    initial commit

---
+++ grep/src/main/java/com/featurerich/grep/GrepHostFileController.java
@@ -0,0 +1,125 @@
+package com.featurerich.grep;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequiredArgsConstructor
+public class GrepHostFileController {
+
+    private final Integer DEFAULT_MAX_TARGET_LENGTH = 8192;
+    private final Pattern variablePattern = Pattern.compile("\\[(.*?)]");
+
+    @GetMapping("/grep/host-file")
+    public ModelAndView getGrepHostFile() {
+
+        ModelAndView model = new ModelAndView("com/featurerich/grep/templates/grep-host-file");
+        model.addObject("grepHostFile", new GrepHostFile());
+
+        return model;
+    }
+
+    @PostMapping("/grep/host-file")
+    public ModelAndView postGrepHostFile(
+            @Valid @ModelAttribute("grepHostFile") GrepHostFile grepHostFile,
+            BindingResult bindingResult) {
+
+        final ModelAndView modelAndView = new ModelAndView();
+
+        if (bindingResult.hasErrors()) {
+
+            modelAndView.setViewName("com/featurerich/grep/templates/grep-host-file");
+            modelAndView.addObject("grepHostFile", grepHostFile);
+
+            return modelAndView;
+        }
+
+        final Matcher matcher = variablePattern.matcher(grepHostFile.getPattern());
+
+        final List<String> variables = new ArrayList<>();
+        final List<List<String>> matches = new ArrayList<>();
+
+        while (matcher.find()) {
+
+            variables.add(matcher.group(1));
+        }
+
+        final Pattern variableMatcher = Pattern.compile(matcher.replaceAll("(.*)"));
+
+        try (final BufferedReader reader =
+                new BufferedReader(new FileReader(grepHostFile.getPath()))) {
+
+            final StringBuilder stringBuilder = new StringBuilder();
+
+            while (true) {
+
+                final int ch = reader.read();
+
+                if (ch == '\r'
+                        || ch == '\n'
+                        || ch == -1
+                        || stringBuilder.length() == DEFAULT_MAX_TARGET_LENGTH) {
+
+                    final Matcher lineMatcher = variableMatcher.matcher(stringBuilder);
+
+                    while (lineMatcher.find()) {
+
+                        final List<String> groups = new ArrayList<>();
+
+                        for (int group = 1; group <= lineMatcher.groupCount(); group++) {
+
+                            groups.add(lineMatcher.group(group));
+                        }
+                        matches.add(groups);
+                    }
+
+                    if (ch == -1) {
+
+                        break;
+                    }
+                    stringBuilder.setLength(0);
+                }
+                stringBuilder.append((char) ch);
+            }
+        } catch (final IOException e) {
+
+            bindingResult.reject(null, e.getMessage());
+            modelAndView.setViewName("com/featurerich/grep/templates/grep-host-file");
+            modelAndView.addObject("grepHostFile", grepHostFile);
+        }
+
+        modelAndView.addObject(grepHostFile);
+        modelAndView.addObject("variables", variables);
+        modelAndView.addObject("matches", matches);
+        modelAndView.setViewName("com/featurerich/grep/templates/grep-host-file");
+
+        return modelAndView;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class GrepHostFile {
+
+        @NotBlank private String path;
+
+        @NotBlank private String pattern;
+    }
+}
