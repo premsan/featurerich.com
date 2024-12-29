bb3db91c341eb1cc7ae06a005be7faa5af8a138d
admin <admin@premsan.com>
2024-12-29 22:38:05 +0900

    test branch

---
+++ payment/src/test/java/com/featurerich/payment/PaymentApplicationTest.java
@@ -0,0 +1,13 @@
+package com.featurerich.payment;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class PaymentApplicationTest {
+
+    @Test
+    void main() {
+
+        Assertions.assertTrue(false);
+    }
+}
