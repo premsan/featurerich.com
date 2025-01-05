c477d8d0089fb591a0c421c70d4defdfc8431368
admin <admin@premsan.com>
2025-01-05 23:32:10 +0900

    application configuration

--- application/src/main/java/com/featurerich/application/ModuleConfigurationController.java
+++ application/src/main/java/com/featurerich/application/ModuleConfigurationController.java
@@ -1,15 +1,29 @@
 package com.featurerich.application;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.text.MessageFormat;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Properties;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,23 +31,33 @@ public class ModuleConfigurationController {
 
     private final Environment environment;
 
-    private String messageFormat = "com/featurerich/{0}/config/{0}.properties";
+    private String baseConfigurationPathFormat = "com/featurerich/{0}/config/{0}.properties";
 
-    @GetMapping("/application/module-configuration/{module}")
-    public ModelAndView moduleConfigurationGet(@PathVariable String module) throws IOException {
+    private String profileConfigurationPathFormat =
+            "{0}/.featurerich/{1}/config/{1}-{2}.properties";
+
+    @GetMapping("/application/module-configuration-update/{moduleId}")
+    public ModelAndView moduleConfigurationGet(@PathVariable String moduleId) throws IOException {
 
         final ModelAndView modelAndView =
-                new ModelAndView("com/featurerich/application/templates/module-configuration-view");
+                new ModelAndView(
+                        "com/featurerich/application/templates/module-configuration-update");
+        modelAndView.addObject("moduleId", moduleId);
 
-        final Properties properties = new Properties();
+        final Map<String, String> properties = new HashMap<>();
 
         try (final InputStream inputStream =
                 getClass()
                         .getClassLoader()
-                        .getResourceAsStream(MessageFormat.format(messageFormat, module))) {
+                        .getResourceAsStream(
+                                MessageFormat.format(baseConfigurationPathFormat, moduleId))) {
 
             final Properties loadedProperties = new Properties();
-            loadedProperties.load(inputStream);
+
+            if (Objects.nonNull(inputStream)) {
+
+                loadedProperties.load(inputStream);
+            }
 
             for (final String key : loadedProperties.stringPropertyNames()) {
 
@@ -41,7 +65,71 @@ public class ModuleConfigurationController {
             }
         }
 
-        modelAndView.addObject("properties", properties);
+        if (properties.isEmpty()) {
+
+            return modelAndView;
+        }
+
+        ModuleConfigurationUpdate moduleConfigurationUpdate = new ModuleConfigurationUpdate();
+        moduleConfigurationUpdate.setProperties(properties);
+
+        modelAndView.addObject("moduleConfigurationUpdate", moduleConfigurationUpdate);
         return modelAndView;
+    }
+
+    @PostMapping("/application/module-configuration-update/{moduleId}")
+    public ModelAndView moduleConfigurationPost(
+            @PathVariable String moduleId,
+            @Valid @ModelAttribute("moduleConfigurationUpdate")
+                    ModuleConfigurationUpdate moduleConfigurationUpdate,
+            BindingResult bindingResult,
+            RedirectAttributes redirectAttributes)
+            throws IOException {
+
+        final ModelAndView modelAndView =
+                new ModelAndView(
+                        "com/featurerich/application/templates/module-configuration-update");
+
+        if (bindingResult.hasErrors()) {
+
+            modelAndView.addObject("moduleId", moduleId);
+            modelAndView.addObject("moduleConfigurationUpdate", moduleConfigurationUpdate);
+
+            return modelAndView;
+        }
+
+        final Properties properties = new Properties();
+        properties.putAll(moduleConfigurationUpdate.getProperties());
+
+        final File profileConfigurationFile =
+                new File(
+                        MessageFormat.format(
+                                profileConfigurationPathFormat,
+                                System.getProperty("user.home"),
+                                moduleId,
+                                environment.getActiveProfiles()[0]));
+
+        if (!profileConfigurationFile.getParentFile().exists()) {
+            profileConfigurationFile.getParentFile().mkdirs();
+        }
+        if (!profileConfigurationFile.exists()) {
+            profileConfigurationFile.createNewFile();
+        }
+        try (final FileOutputStream fileOutputStream =
+                new FileOutputStream(profileConfigurationFile)) {
+
+            properties.store(fileOutputStream, null);
+        }
+
+        redirectAttributes.addAttribute("moduleId", moduleId);
+        return new ModelAndView("redirect:/application/module-configuration-update/{moduleId}");
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public class ModuleConfigurationUpdate {
+
+        @NotNull private Map<String, String> properties;
     }
 }
